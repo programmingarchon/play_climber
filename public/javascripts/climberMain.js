efe12b84cc7a2777d1a3e3e91df84b1cf851ee4f
ProgrammingArchon <programmingarchon@gmail.com>
2013-05-18 16:09:59 +0300

    Added block and basic gravity

--- public/javascripts/climberMain.js
+++ public/javascripts/climberMain.js
@@ -1,26 +1,56 @@
 enchant();
 
+
+Character = Class.create(Sprite, {
+    initialize: function() {
+        Sprite.call(this, 16, 16);
+        this.image = game.assets["../assets/images/climber/character.png"];
+        this.x = 0;
+        this.y = 260;
+        this.moveSpeed = 2;
+        this.gravitySpeed = 2;
+    },
+
+    onenterframe: function() {
+        if(game.input.left && !game.input.right){
+            this.x -= this.moveSpeed;
+        } else if(game.input.right && !game.input.left){
+            this.x += this.moveSpeed;
+        }
+        this.y += this.gravitySpeed;
+    }
+});
+
+Block = Class.create(Sprite, {
+    initialize: function() {
+        Sprite.call(this, 16, 16);
+        this.image = game.assets["../assets/images/climber/block.png"];
+        this.x = 0;
+        this.y = 290;
+    },
+
+    onenterframe: function() {
+        if (this.intersect(character)) {
+            character.gravitySpeed = 0;
+        }
+    }
+});
+
 window.onload = function(){
-    var game = new Core(320, 320);
+    game = new Core(320, 320);
     game.fps = 15;
-    game.preload("../assets/images/chara1.png");
-    game.onload = function(){
-        var bear = new Sprite(32, 32);
-        bear.image = game.assets["../assets/images/chara1.png"];
-        bear.x = 0;
-        bear.y = 0;
-        bear.frame = 5;
-        game.rootScene.addChild(bear);
+    game.preload("../assets/images/climber/character.png");
+    game.preload("../assets/images/climber/block.png");
+    game.keybind(65, 'left');
+    game.keybind(68, 'right');
+    game.keybind(87, 'up');
+    game.keybind(83, 'down');
+    game.onload = function() {
+        character = new Character();
+        var block = new Block();
+        game.rootScene.addChild(character);
+        game.rootScene.addChild(block);
 
-        bear.addEventListener("enterframe", function(){
-            this.x += 1;
-            this.frame = this.age % 2 + 6;
-            this.y += 1;
-        });
-
-        bear.addEventListener("touchstart", function(){
-            game.rootScene.removeChild(bear);
-        });
     };
     game.start();
 };
